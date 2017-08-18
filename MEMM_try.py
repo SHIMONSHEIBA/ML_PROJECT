227e55daabbc153c8b7d6d63e5402c9ca58f8990
shimo <shimonsheiba@gmail.com>
2017-08-18 20:44:13 +0300

    finished features

--- MEMM_try.py
+++ MEMM_try.py
@@ -17,6 +17,8 @@ class MEMM:
 
     stop_keys = ['TGA','TAA', 'TAG']
 
+    start_keys = ['ATG']
+
     tags_dict = {'1' : [0,'A+'] , '2' : [0,'C+'] ,'3' : [0,'G+'] , '4' : [0,'T+'] ,'5' : [0,'A-'] , '6' : [0,'C-'] ,
                  '7' : [0,'G-'] , '8' : [0,'T-'] }
 
@@ -27,11 +29,12 @@ class MEMM:
         self.training_file = trainingfile
         self.feature_1 = {}
         self.feature_2 = {}
-        self.feature_3 = {}  # feature
-        self.feature_4 = {}  # feature
-        self.feature_5 = {}  # feature
-        self.feature_6 = {}  # feature
-        self.feature_7 = {}  # feature
+        self.feature_3 = {}
+        self.feature_4 = {}
+        self.feature_5 = {}
+        self.feature_6 = {}
+        self.feature_7 = {}
+        self.feature_8 = {}
 
         self.build_features_from_train()
 
@@ -54,6 +57,13 @@ class MEMM:
                 first_tag = '#'
                 second_tag = '#'
 
+                zero_word= ''
+                first_word = ''
+                second_word = ''
+                plus_one_word = ''
+                plus_two_word = ''
+                plus_three_word = ''
+
                 for word_in_seq_index , word_tag in enumerate(word_tag_list):
 
                     word_tag_tuple = word_tag.split('_')
@@ -67,8 +77,8 @@ class MEMM:
                     current_word = word_tag_tuple[0]
                     current_tag = word_tag_tuple[1]
 
-                    feature_3_key = None
-                    feature_4_key = None
+                    feature_3_key = ''
+                    feature_4_key = ''
 
                     # build feature_1 of three tags instances
                     feature_1_key = first_tag + second_tag + current_tag
@@ -91,22 +101,24 @@ class MEMM:
                         feature_4_key =  self.amino_mapping[feature_3_key]
 
                     # build feature_3 of three words instances
-                    if feature_3_key not in self.feature_3:
-                        self.feature_3[feature_3_key] = 1
-                    else:
-                        self.feature_3[feature_3_key] += 1
+                    if feature_3_key:
+                        if feature_3_key not in self.feature_3:
+                            self.feature_3[feature_3_key] = 1
+                        else:
+                            self.feature_3[feature_3_key] += 1
 
                     # build feature_4 of amino acids instances
-                    if feature_4_key not in self.feature_4:
-                        self.feature_4[feature_4_key] = 1
-                    else:
-                        self.feature_4[feature_4_key] += 1
+                    if feature_4_key:
+                        if feature_4_key not in self.feature_4:
+                            self.feature_4[feature_4_key] = 1
+                        else:
+                            self.feature_4[feature_4_key] += 1
 
-                    # build feature_5 of stop before current word
+                    # build feature_5 of stop codon before current word
                     if word_in_seq_index > 2:
                         zero_word = word_tag_list[word_in_seq_index-3][0]
-                        first_word = word_tag_list[word_in_seq_index-2][0]
-                        second_word = word_tag_list[word_in_seq_index-1][0]
+                        #first_word = word_tag_list[word_in_seq_index-2][0]
+                        #second_word = word_tag_list[word_in_seq_index-1][0]
                         feature_5_key = zero_word + first_word + second_word
                         if feature_5_key in self.stop_keys:
                             if feature_5_key not in self.feature_5:
@@ -114,14 +126,49 @@ class MEMM:
                             else:
                                 self.feature_5[feature_5_key] += 1
 
+                    # build feature_6 of stop codon after current word
+                    if len(word_tag_list)-word_in_seq_index > 3:
+                        plus_one_word = word_tag_list[word_in_seq_index + 1][0]
+                        plus_two_word = word_tag_list[word_in_seq_index + 2][0]
+                        plus_three_word = word_tag_list[word_in_seq_index + 3][0]
+                        feature_6_key = plus_one_word + plus_two_word + plus_three_word
+                        if feature_6_key in self.stop_keys:
+                            if feature_6_key not in self.feature_6:
+                                self.feature_6[feature_6_key] = 1
+                            else:
+                                self.feature_6[feature_6_key] += 1
+                    # TODO: validate words are updated per itaration
+                    # build feature_7 of start codon before current word
+                    if word_in_seq_index > 2:
+                        #zero_word = word_tag_list[word_in_seq_index - 3][0]
+                        # first_word = word_tag_list[word_in_seq_index-2][0]
+                        # second_word = word_tag_list[word_in_seq_index-1][0]
+                        feature_7_key = zero_word + first_word + second_word
+                        if feature_7_key in self.start_keys:
+                            if feature_7_key not in self.feature_7:
+                                self.feature_7[feature_7_key] = 1
+                            else:
+                                self.feature_7[feature_7_key] += 1
+
+                    # build feature_8 of start codon after current word
+                    if len(word_tag_list) - word_in_seq_index > 3:
+                        #plus_one_word = word_tag_list[word_in_seq_index + 1][0]
+                        #plus_two_word = word_tag_list[word_in_seq_index + 2][0]
+                        #plus_three_word = word_tag_list[word_in_seq_index + 3][0]
+                        feature_8_key = plus_one_word + plus_two_word + plus_three_word
+                        if feature_8_key in self.start_keys:
+                            if feature_8_key not in self.feature_8:
+                                self.feature_8[feature_8_key] = 1
+                            else:
+                                self.feature_8[feature_8_key] += 1
 
 
-
-
-
-
-
-
+                    # update words
+                    #zero_word = first_word
+                    #first_word = second_word
+                    #second_word = current_word
+                    #plus_one_word = plus_two_word
+                    #plus_two_word = plus_three_word
 
                     # update tags
                     first_tag = second_tag
