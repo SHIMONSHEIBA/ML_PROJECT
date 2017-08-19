bb25d3499d65891e00c99c6ea3da896c5c374ff6
shimo <shimonsheiba@gmail.com>
2017-08-19 16:31:23 +0300

    BUILD FEATURE VECTOR FUNCTION

--- testing.py
+++ testing.py
@@ -1,11 +1,20 @@
-key = ''
 
-if key:
-    print('good')
 
-a='a'
-b='b'
-key = a+b
+features_vector = {}
+features_vector_mapping = {}
+index = 0
 
-if key:
-    print(key)
+word_tag_dict = {'A': ['1', '5'], 'C': ['2', '6'], 'G': ['3', '7'], 'T': ['4', '8']}
+for word, tag_list in word_tag_dict.items():
+    for tag in tag_list:
+        key = word + '_' + tag
+        features_vector[key] = index
+        features_vector_mapping[index] = key
+        index += 1
+
+
+print(features_vector)
+print(features_vector_mapping)
+
+
+
