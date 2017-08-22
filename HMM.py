bbe7888bf9c95e09851aba0f7755b20b653806d4
user <reutapel88@gmail.com>
2017-08-22 20:24:21 +0300

    Fix bugs in hmm, viterbi_ML and print

--- HMM.py
+++ HMM.py
@@ -1,8 +1,6 @@
-import numpy as np
-import math
-from time import time
 import time
 from itertools import product
+import itertools
 
 
 class HMM(object):
@@ -20,19 +18,20 @@ class HMM(object):
         self.transition_mat = {}
         self.emission_mat = {}  # emission matrix: the probability to see base with tag (e.g: P(A|1))
         # self.initial_mat = {}  # initial matrix:
-        self.three_tags_dict = declare_dict(order=3)  # number of instances of three tags together
-        self.two_tags_dict = declare_dict(order=2)  # number of instances of two tags together
-        # number of instances of each tag
-        self.one_tags_dict = {'1': [0, 'A+'], '2': [0, 'C+'], '3': [0, 'G+'], '4': [0, 'T+'], '5': [0, 'A-'],
-                              '6': [0, 'C-'], '7': [0, 'G-'], '8': [0, 'T-']}
         # number of instances of a word and a tag together
         self.word_tag_dict_count =\
-            {['A_1']: 0, ['A_2']: 0, ['A_3']: 0, ['A_4']: 0, ['A_5']: 0, ['A_6']: 0, ['A_7']: 0, ['A_8']: 0,
-             ['C_1']: 0, ['C_2']: 0, ['C_3']: 0, ['C_4']: 0, ['C_5']: 0, ['C_6']: 0, ['C_7']: 0, ['C_8']: 0,
-             ['G_1']: 0, ['G_2']: 0, ['G_3']: 0, ['G_4']: 0, ['G_5']: 0, ['G_6']: 0, ['G_7']: 0, ['G_8']: 0,
-             ['T_1']: 0, ['T_2']: 0, ['T_3']: 0, ['T_4']: 0, ['T_5']: 0, ['T_6']: 0, ['T_7']: 0, ['T_8']: 0}
+            {'A_1': 0, 'A_2': 0, 'A_3': 0, 'A_4': 0, 'A_5': 0, 'A_6': 0, 'A_7': 0, 'A_8': 0,
+             'C_1': 0, 'C_2': 0, 'C_3': 0, 'C_4': 0, 'C_5': 0, 'C_6': 0, 'C_7': 0, 'C_8': 0,
+             'G_1': 0, 'G_2': 0, 'G_3': 0, 'G_4': 0, 'G_5': 0, 'G_6': 0, 'G_7': 0, 'G_8': 0,
+             'T_1': 0, 'T_2': 0, 'T_3': 0, 'T_4': 0, 'T_5': 0, 'T_6': 0, 'T_7': 0, 'T_8': 0}
 
-        self.word_tag_dict = {'A': ['1', '5'], 'C': ['2', '6'], 'G': ['3', '7'], 'T': ['4', '8']}
+        self.word_tag_dict = {'A': ['1', '5'], 'C': ['2', '6'], 'G': ['3', '7'], 'T': ['4', '8'], '#': ['#']}
+
+        self.three_tags_dict = self.declare_dict(order=3)  # number of instances of three tags together
+        self.two_tags_dict = self.declare_dict(order=2)  # number of instances of two tags together
+        # number of instances of each tag
+        self.one_tags_dict = {'1': [0, 'A+'], '2': [0, 'C+'], '3': [0, 'G+'], '4': [0, 'T+'], '5': [0, 'A-'],
+                              '6': [0, 'C-'], '7': [0, 'G-'], '8': [0, 'T-'], '#': [0, '#']}
 
         self.training_file = train_file
         self.lambda1 = lambda1
@@ -45,51 +44,11 @@ class HMM(object):
         self.create_transition_matrix()
         self.create_emission_matrix()
 
-    # def create_hmm(self):
-    #     self.states = sorted(list(self.states))
-    #     self.words = sorted(list(self.words))
-    #
-    #     # create maps from state labels / emission symbols to integers that function as unique IDs
-    #     qmap, smap = {}, {}
-    #     for i in xrange(len(self.states)):
-    #         qmap[self.states[i]] = i
-    #     for i in xrange(len(self.words)):
-    #         smap[self.words[i]] = i
-    #     lenq = len(self.states)
-    #
-    #     # create and populate transition probability matrix
-    #     self.A = np.zeros(shape=(lenq, lenq), dtype=float)
-    #     for a, prob in A.iteritems():
-    #         asrc, adst = a[0], a[1]
-    #         self.A[qmap[asrc], qmap[adst]] = prob
-    #     # make A stochastic (i.e. make rows add to 1)
-    #     self.A /= self.A.sum(axis=1)[:, np.newaxis]
-    #
-    #     # create and populate emission probability matrix
-    #     self.E = np.zeros(shape=(lenq, len(self.words)), dtype=float)
-    #     for e, prob in E.iteritems():
-    #         eq, es = e[0], e[1]
-    #         self.E[qmap[eq], smap[es]] = prob
-    #     # make E stochastic (i.e. make rows add to 1)
-    #     self.E /= self.E.sum(axis=1)[:, np.newaxis]
-    #
-    #     # initial probabilities
-    #     self.I = [ 0.0 ] * len(self.states)
-    #     for a, prob in I.iteritems():
-    #         self.I[qmap[a]] = prob
-    #     # make I stochastic (i.e. adds to 1)
-    #     self.I = np.divide(self.I, sum(self.I))
-    #
-    #     self.qmap, self.smap = qmap, smap
-    #
-    #     # Make log-base-2 versions for log-space functions
-    #     self.Alog = np.log2(self.A)
-    #     self.Elog = np.log2(self.E)
-    #     self.Ilog = np.log2(self.I)
-
     def create_dictionaries(self):
         print '{}: Start build transition and emission matrices'.format(time.asctime(time.localtime(time.time())))
         with open(self.training_file) as training:
+            sequence_index = 0
+
             for sequence in training:
                 word_tag_list = sequence.split(',')
                 # define two first word_tags for some features
@@ -99,6 +58,7 @@ class HMM(object):
                     word_tag_tuple = word_tag.split('_')
                     if '\n' in word_tag_tuple[1]:
                         word_tag_tuple[1] = word_tag_tuple[1][:1]
+                        word_tag = word_tag_tuple[0] + '_' + word_tag_tuple[1]
 
                     current_tag = word_tag_tuple[1]
 
@@ -110,21 +70,21 @@ class HMM(object):
 
                     # count number of instances of two tags together
                     two_tags = second_tag + '_' + current_tag
-                    if two_tags not in self.two_tags_dict:
-                        self.two_tags_dict[two_tags] = 1
-                    else:
-                        self.two_tags_dict[two_tags] += 1
+                    self.two_tags_dict[two_tags] += 1
 
                     # count number of instances of three tags together
                     three_tags = first_tag + '_' + second_tag + '_' + current_tag
-                    if three_tags not in self.three_tags_dict:
-                        self.three_tags_dict[three_tags] = 1
-                    else:
-                        self.three_tags_dict[three_tags] += 1
+                    self.three_tags_dict[three_tags] += 1
 
                     # update tags
                     first_tag = second_tag
                     second_tag = current_tag
+
+                sequence_index += 1
+
+            self.two_tags_dict['#_#'] = sequence_index
+            self.one_tags_dict['#'][0] = sequence_index
+
         print '{}: Finish build transition and emission matrices'.format(time.asctime(time.localtime(time.time())))
 
         return
@@ -135,14 +95,21 @@ class HMM(object):
             tags = three_tags.split('_')
             # calculate all counts to calculate the probability
             count_first_two_tags = self.two_tags_dict[tags[0] + '_' + tags[1]]
-            count_last_two_tags = self.two_tags_dict[tags[0] + '_' + tags[1]]
-            count_second_tag = self.one_tags_dict[tags[1]]
-            count_third_tag = self.one_tags_dict[tags[2]]
+            count_last_two_tags = self.two_tags_dict[tags[1] + '_' + tags[2]]
+            count_second_tag = self.one_tags_dict[tags[1]][0]
+            count_third_tag = self.one_tags_dict[tags[2]][0]
             # calculate probability P(word3|word1,word2)
+            if count_first_two_tags != 0:
+                first_composed = float(count_all_three_tags) / float(count_first_two_tags)
+            else:
+                first_composed = 0.0
+            if count_second_tag != 0:
+                second_composed = float(count_last_two_tags) / float(count_second_tag)
+            else:
+                second_composed = 0.0
+            third_composed = float(count_third_tag) / float(count_all_tags_instances)
             self.transition_mat[tags[2] + '|' + tags[0] + ',' + tags[1]] = \
-                (self.lambda1 * count_all_three_tags / count_first_two_tags) + \
-                (self.lambda2 * count_last_two_tags / count_second_tag) + \
-                (self.lambda3 * count_third_tag / count_all_tags_instances)
+                (self.lambda1 * first_composed) + (self.lambda2 * second_composed) + (self.lambda3 * third_composed)
 
     def create_emission_matrix(self):
         for word_tag, count_word_tag in self.word_tag_dict_count.items():
@@ -151,28 +118,30 @@ class HMM(object):
             self.emission_mat[word_tag_list[0] + '|' + word_tag_list[1]] = 1.0 * count_word_tag / count_tag
 
 
-def declare_dict(order):
-    permutations_list = product('12345678', repeat=order)
-    dictionary = {}
-    # create key for all permutation of size order
-    for permutation in permutations_list:
-        if order == 2:
-            key = permutation[0] + '_' + permutation[1]
-        else:
-            key = permutation[0] + '_' + permutation[1] + '_' + permutation[2]
-        dictionary[key] = 0
-    # create key for first word in two_tags_dict (i.e: #_word)
-    if order == 2:
-        for word in ['A', 'C', 'G', 'T']:
-            dictionary['#_' + word] = 0
-    # create key for first word in three_tags_dict (i.e: #_#_word)
-    else:
-        for word in ['A', 'C', 'G', 'T']:
-            dictionary['#_#_' + word] = 0
-        # create key for second word in three_tags_dict (i.e: #_word1_word2)
-        permutations_list = product('12345678', repeat=order-1)
+    def declare_dict(self, order):
+        permutations_list = product('12345678', repeat=order)
+        dictionary = {}
+        # create key for all permutation of size order
         for permutation in permutations_list:
-            key = '#_' + permutation[0] + '_' + permutation[1]
+            if order == 2:
+                key = permutation[0] + '_' + permutation[1]
+            else:
+                key = permutation[0] + '_' + permutation[1] + '_' + permutation[2]
             dictionary[key] = 0
+        # create key for first word in two_tags_dict (i.e: #_word)
+        tag_list = list(itertools.chain.from_iterable(self.word_tag_dict.values()))
+        if order == 2:
+            for word in tag_list:
+                dictionary['#_' + word] = 0
+            dictionary['#_#'] = 0
+        # create key for first word in three_tags_dict (i.e: #_#_word)
+        else:
+            for word in tag_list:
+                dictionary['#_#_' + word] = 0
+            # create key for second word in three_tags_dict (i.e: #_word1_word2)
+            permutations_list = product('12345678', repeat=order-1)
+            for permutation in permutations_list:
+                key = '#_' + permutation[0] + '_' + permutation[1]
+                dictionary[key] = 0
 
-    return dictionary
+        return dictionary
--- main_hmm.py
+++ main_hmm.py
@@ -1,14 +1,13 @@
 from HMM import HMM
 from viterbi_ML import viterbi
-import numpy as np
-import datetime
 import time
 from Print_and_save_results import print_save_results
 
 
 def main():
     chrome_number = '1'
-    train_file = 'C:\\gitprojects\\ML project\\samples_small_data\\' + chrome_number + '_label.csv'
+    # train_file = 'C:\\gitprojects\\ML_PROJECT\\labels\\chr' + chrome_number + '_label.csv'
+    train_file = 'C:/gitprojects/ML project/samples_small_data/label_small.csv'
     print '{}: Start creating HMM'.format((time.asctime(time.localtime(time.time()))))
     lambda1 = 0.2
     lambda2 = 0.5
@@ -16,13 +15,16 @@ def main():
     hmm = HMM(train_file, lambda1, lambda2, lambda3)
 
     print '{}: Start viterbi'.format((time.asctime(time.localtime(time.time()))))
-    viterbi_obj = viterbi(hmm, 'hmm', train_file)
+    viterbi_obj = viterbi(hmm, 'hmm', train_file, is_log=True)
     viterbi_result = viterbi_obj.viterbi_all_data()
 
     print 'start evaluation'
-    write_file_name = 'C:\gitprojects\ML_PROJECT\\file_results\\chr' + chrome_number + '_result.csv'
-    confusion_file_name = 'C:\gitprojects\ML_PROJECT\\confusion_files\\chr' + chrome_number + '_CM.xls'
-    evaluate_obj = print_save_results(hmm, 'hmm', train_file, viterbi_result, write_file_name, confusion_file_name)
+    write_file_name = 'C:\\gitprojects\\ML_PROJECT\\file_results\\chr' + chrome_number + '_result.csv'
+    confusion_file_name = 'C:\\gitprojects\\ML_PROJECT\\confusion_files\\chr' + chrome_number + '_CM.xls'
+    seq_confusion_file_name = 'C:\\gitprojects\\ML_PROJECT\\confusion_files\\chr' + chrome_number + '_sqeCM.xls'
+    seq_labels_file_name = 'C:/gitprojects/ML project/samples_small_data/chr1_sample_label.xlsx'
+    evaluate_obj = print_save_results(hmm, 'hmm', train_file, viterbi_result, write_file_name,
+                                      confusion_file_name, seq_labels_file_name, seq_confusion_file_name)
     evaluate_result = evaluate_obj.run()
 
     print evaluate_result
